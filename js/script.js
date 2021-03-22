23d455317f3da7f56e662a5974087670c901ad23
MirkoGriffo <mirkogriffo@yahoo.it>
2021-03-22 17:25:56 +0100

    Esercizio 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+/*
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, 
+stampa un messaggio appropriato sull’esito del controllo.
+*/
 
+var mail = ["mirko@gmail.com", "christian@gmail.com", "mauro@gmail.com"];
+
+var mailInserita = prompt("Inserire email");
+
+utenteTrovato = false;
+
+for (var i = 0; i < mail.length; i++) {
+  if (mailInserita === mail[i]) {
+    utenteTrovato = true;
+  }
+}
+
+if (utenteTrovato === true) {
+  document.getElementById("avviso").innerHTML = "Accesso effettuato";
+} else {
+  document.getElementById("avviso").innerHTML = "Accesso negato";
+}
